6d6800cb9f68c2604cb16948cbf08e952b83e7c7
Milad salamian <miladsalami1385@gmail.com>
2026-07-05 20:16:05 +0330

    add crud oparation in service module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,116 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto, GetUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { UserRoleEnums } from './enums/userRoleEnums';
+import bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const isUserExist = await this.findOneByMobile(
+        createUserDto.mobile,
+        createUserDto.email,
+        createUserDto.username,
+      );
+      if (isUserExist) {
+        throw new BadRequestException(
+          'کاربری با این اطلاعات در سایت وجود دارد ',
+        );
+      }
+      const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+      const role =
+        (await this.userRepository.count({})) === 0
+          ? UserRoleEnums.ADMIN
+          : UserRoleEnums.USER;
+      const newUser = this.userRepository.create({
+        ...createUserDto,
+        role,
+        password: hashedPassword,
+      });
+      return await this.userRepository.save(newUser);
+    } catch (error) {
+      throw error;
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll({
+    page = 1,
+    limit = 10,
+    status,
+  }: GetUserDto): Promise<{ users: User[]; count: number }> {
+    try {
+      let where: any = {};
+      if (status) {
+        where.status = status;
+      }
+      const count = await this.userRepository.count({});
+      const users = await this.userRepository.find({
+        where,
+        skip: (page - 1) * limit,
+        take: limit,
+      });
+
+      return {
+        users,
+        count,
+      };
+    } catch (error) {
+      throw new BadRequestException('هنگام دریافت کاربران مشکلی به وجود آمد');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOneByMobile(mobile: string, email: string, username) {
+    const user = await this.userRepository.findOne({
+      where: [{ mobile }, { email }, { username }],
+    });
+    return user;
+  }
+  async findOne(id: number): Promise<User> {
+    try {
+      const user = await this.userRepository.findOne({ where: { id } });
+      if (!user) {
+        throw new BadRequestException(`کاربر با آیدی ${id} یافت نشد`);
+      }
+
+      return user;
+    } catch (error) {
+      throw error;
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    try {
+      await this.findOne(id);
+      const { display_name, role } = updateUserDto;
+      const updatedUser = await this.userRepository.update(id, {
+        display_name,
+        role,
+      });
+      if (updatedUser.affected === 0) {
+        throw new BadRequestException('هنگام آپدیت تسک مشکلی به وجود آمد');
+      }
+
+      return await this.findOne(id);
+    } catch (error) {
+      throw error;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    const deletedUser = await this.userRepository.delete(id);
+
+    if (deletedUser.affected === 0) {
+      throw new BadRequestException('هنگام حذف کاربر مشکلی پیش آمد');
+    }
+
+    return user;
   }
 }
