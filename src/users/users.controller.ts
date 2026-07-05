cf1e2acce28ab4cf938e2c30e86029e27a80ac53
Milad salamian <miladsalami1385@gmail.com>
2026-07-05 20:16:23 +0330

    add user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,34 +1,87 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  HttpStatus,
+  Query,
+  Put,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, GetUserDto, GetUserIdDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import type { Response } from 'express';
+import { createPagination } from 'utils/func';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Res() res: Response, @Body() createUserDto: CreateUserDto) {
+    const user = await this.usersService.create(createUserDto);
+    return res.status(HttpStatus.CREATED).json({
+      statusCode: HttpStatus.CREATED,
+      message: 'کاربر با موفقیت ساخته شد',
+      data: user,
+    });
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll(@Res() res: Response, @Query() queryDto: GetUserDto) {
+    const { page, limit } = queryDto;
+    const { users, count } = await this.usersService.findAll(queryDto);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'کاربران با موفقیت دریافت شد',
+      data: {
+        users,
+        pagination: createPagination(page, limit, count, 'Users'),
+      },
+    });
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  async findOne(@Res() res: Response, @Param() params: GetUserIdDto) {
+    const { id } = params;
+    const user = await this.usersService.findOne(id);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'کاربر با موفقیت دریافت شد',
+      data: user,
+    });
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @Put(':id')
+  async update(
+    @Res() res: Response,
+    @Param() params: GetUserIdDto,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    const { id } = params;
+    const updatedUser = await this.usersService.update(id, updateUserDto);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'کاربر با موفقیت آپدیت شد',
+      data: updatedUser,
+    });
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  async remove(@Res() res: Response, @Param() params: GetUserIdDto) {
+    const { id } = params;
+    const deletedUser = await this.usersService.remove(id);
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'کاربر با موفقیت حذف شد',
+      data: deletedUser,
+    });
   }
 }
