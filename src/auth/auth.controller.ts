6ca0144477e67f327274b36f54d396ce227d59fd
Milad salamian <miladsalami1385@gmail.com>
2026-07-08 23:30:30 +0330

    add get me route in auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,17 +8,31 @@ import {
   Delete,
   Res,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register-auth.dto';
 import { LoginDto } from './dto/login-auth.dto';
 import type { Response } from 'express';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post("register")
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  async getMe(@Res() res: Response, @GetUser() user: User) {
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'اطلاعات شما با موفقیت دریافت شد',
+      data: user,
+    });
+  }
+
+  @Post('register')
   async register(@Res() res: Response, @Body() registerDto: RegisterDto) {
     const register = await this.authService.register(registerDto);
     return res.status(HttpStatus.CREATED).json({
@@ -28,7 +42,7 @@ export class AuthController {
     });
   }
 
-  @Post("login")
+  @Post('login')
   async login(@Res() res: Response, @Body() loginDto: LoginDto) {
     const login = await this.authService.login(loginDto);
     return res.status(HttpStatus.OK).json({
