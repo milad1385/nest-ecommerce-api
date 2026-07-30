d83ae15baa6647614cf0ddb4a031765b14cd34ee
Milad salamian <miladsalami1385@gmail.com>
2026-07-31 01:09:04 +0330

    add product entity

---
+++ src/proudcts/entities/proudct.entity.ts
@@ -0,0 +1,48 @@
+import { Category } from 'src/categories/entities/category.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'products' })
+export class Proudct {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  shortDescription: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @ManyToMany(() => Category, (category) => category.products)
+  @JoinTable({
+    name: 'product_category',
+    joinColumn: {
+      name: 'product_id',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'category_id',
+      referencedColumnName: 'id',
+    },
+  })
+  categories: Category[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
