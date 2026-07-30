ce43c2af6f59419c1d40ef0d6eae88cfc1dc91b3
Milad salamian <miladsalami1385@gmail.com>
2026-07-30 16:47:47 +0330

    add create category controller

---
+++ src/categories/categories.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+import type { Response } from 'express';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  async create(
+    @Res() res: Response,
+    @Body() createCategoryDto: CreateCategoryDto,
+  ) {
+    const category = await this.categoriesService.create(createCategoryDto);
+
+    return res.status(HttpStatus.CREATED).json({
+      statusCode: HttpStatus.CREATED,
+      message: 'دسته بندی با موفقیت ساخته شد',
+      data: category,
+    });
+  }
+
+  @Get()
+  findAll() {
+    return this.categoriesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('slug') slug: string) {
+    return this.categoriesService.findOne(slug);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id') id: string,
+    @Body() updateCategoryDto: UpdateCategoryDto,
+  ) {
+    return this.categoriesService.update(+id, updateCategoryDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.categoriesService.remove(+id);
+  }
+}
