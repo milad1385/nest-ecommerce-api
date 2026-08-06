5c19050cb5f471b29067936880b897f1e0074ab1
Milad salamian <miladsalami1385@gmail.com>
2026-08-06 16:40:09 +0330

    add seller dto

---
+++ src/sellers/dto/create-seller.dto.ts
@@ -0,0 +1,92 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+import { SellerStatusEnums } from '../enums/sellerStatusEnums.enum';
+
+export class CreateSellerDto {
+  @IsNotEmpty({
+    message: 'نام فروشنده الزامی است',
+  })
+  @IsString({
+    message: 'نام فروشنده باید متن باشد',
+  })
+  @MinLength(3, {
+    message: 'نام فروشنده باید حداقل ۳ کاراکتر باشد',
+  })
+  @MaxLength(100, {
+    message: 'نام فروشنده نباید بیشتر از ۱۰۰ کاراکتر باشد',
+  })
+  name: string;
+
+  @IsNotEmpty({
+    message: 'شماره تلفن الزامی است',
+  })
+  @IsPhoneNumber('IR', {
+    message: 'شماره تلفن معتبر نیست (مثال: ۰۹۱۲۱۲۳۴۵۶۷)',
+  })
+  phone: string;
+
+  @IsNotEmpty({
+    message: 'ایمیل الزامی است',
+  })
+  @IsEmail(
+    {},
+    {
+      message: 'ایمیل معتبر نیست (مثال: example@gmail.com)',
+    },
+  )
+  email: string;
+
+  @IsNotEmpty({
+    message: 'کد پستی الزامی است',
+  })
+  @IsNumber(
+    {},
+    {
+      message: 'کد پستی باید عدد باشد',
+    },
+  )
+  @Min(1000000000, {
+    message: 'کد پستی باید ۱۰ رقم باشد (مثال: 3178884565)',
+  })
+  postal_code: number;
+
+  @IsNotEmpty({
+    message: 'استان الزامی است',
+  })
+  @IsString({
+    message: 'استان باید متن باشد',
+  })
+  @MaxLength(50, {
+    message: 'نام استان نباید بیشتر از ۵۰ کاراکتر باشد',
+  })
+  province: string;
+
+  @IsOptional()
+  @IsEnum(SellerStatusEnums, {
+    message:
+      'وضعیت فروشنده معتبر نیست. مقادیر مجاز: ${Object.values(SellerStatusEnums).join(", ")}',
+  })
+  status?: SellerStatusEnums = SellerStatusEnums.PENDING;
+
+  @IsOptional()
+  @IsNumber(
+    {},
+    {
+      message: 'شناسه کاربر باید عدد باشد',
+    },
+  )
+  @Min(1, {
+    message: 'شناسه کاربر معتبر نیست',
+  })
+  userId: number;
+}
