99db7c2cf40a07cf697bc7e197791febe129caf1
Milad salamian <miladsalami1385@gmail.com>
2026-08-07 18:35:42 +0330

    init entity in seller requests

--- src/sellers-requests/sellers-requests.module.ts
+++ src/sellers-requests/sellers-requests.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SellersRequestsService } from './sellers-requests.service';
 import { SellersRequestsController } from './sellers-requests.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SellersRequest } from './entities/sellers-request.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SellersRequest])],
   controllers: [SellersRequestsController],
   providers: [SellersRequestsService],
 })
