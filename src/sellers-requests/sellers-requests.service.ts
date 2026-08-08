779acfc9fdf1ee6bf04f16eb3baa3cbc7e0ded0e
Milad salamian <miladsalami1385@gmail.com>
2026-08-08 12:24:25 +0330

    add create seller request service

--- src/sellers-requests/sellers-requests.service.ts
+++ src/sellers-requests/sellers-requests.service.ts
@@ -1,11 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateSellersRequestDto } from './dto/create-sellers-request.dto';
 import { UpdateSellersRequestDto } from './dto/update-sellers-request.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { SellersRequest } from './entities/sellers-request.entity';
+import { Repository } from 'typeorm';
+import { SellersService } from 'src/sellers/sellers.service';
+import { SellerRequestEnums } from './enums/sellers-requests-status-enums';
+import { ProudctsService } from 'src/proudcts/proudcts.service';
 
 @Injectable()
 export class SellersRequestsService {
-  create(createSellersRequestDto: CreateSellersRequestDto) {
-    return 'This action adds a new sellersRequest';
+  constructor(
+    @InjectRepository(SellersRequest)
+    private readonly sellerRequestRepository: Repository<SellersRequest>,
+    private readonly sellerService: SellersService,
+    private readonly productService: ProudctsService,
+  ) {}
+  async create(
+    createSellersRequestDto: CreateSellersRequestDto,
+    userId: number,
+  ) {
+    const seller = await this.sellerService.findByUserId(userId);
+    const { product_id, price, stock, priority, discount } =
+      createSellersRequestDto;
+
+    const sellerRequest = await this.sellerRequestRepository.findOne({
+      where: {
+        status: SellerRequestEnums.PENDING,
+        seller: { id: seller.id },
+        product: { id: product_id },
+      },
+    });
+
+    if (sellerRequest) {
+      throw new BadRequestException(
+        'شما برای این محصول درخواست ثبت کرده اید لطفا منتظر نتیجه باشید',
+      );
+    }
+
+    const isAcceptRequest = await this.sellerRequestRepository.findOne({
+      where: {
+        status: SellerRequestEnums.ACCEPT,
+        seller: { id: seller.id },
+        product: { id: product_id },
+      },
+    });
+
+    if (isAcceptRequest) {
+      throw new BadRequestException(
+        'شما برای این محصول درخواست ثبت کرده اید و تایید شده است',
+      );
+    }
+
+    const product = await this.productService.findOneById(product_id);
+
+    const newSellerRequest = this.sellerRequestRepository.create({
+      product,
+      seller,
+      price,
+      discount,
+      stock,
+      priority,
+    });
+
+    return await this.sellerRequestRepository.save(newSellerRequest);
   }
 
   findAll() {
