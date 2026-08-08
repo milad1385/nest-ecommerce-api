74082e2c99edc0bc55fbe03cc2ca81ed32fe081f
Milad salamian <miladsalami1385@gmail.com>
2026-08-08 12:57:10 +0330

    fix update seller request dto

--- src/sellers-requests/dto/update-sellers-request.dto.ts
+++ src/sellers-requests/dto/update-sellers-request.dto.ts
@@ -1,14 +1,10 @@
+import { Type } from 'class-transformer';
 import {
-  IsEnum,
   IsNumber,
   IsOptional,
-  IsString,
   Max,
-  Min,
-  MinLength,
+  Min
 } from 'class-validator';
-import { Type } from 'class-transformer';
-import { SellerRequestEnums } from '../enums/sellers-requests-status-enums';
 
 export class UpdateSellersRequestDto {
   @IsOptional()
@@ -31,20 +27,9 @@ export class UpdateSellersRequestDto {
   discount?: number;
 
   @IsOptional()
-  @IsEnum(SellerRequestEnums, {
-    message: `وضعیت باید یکی از مقادیر ${Object.values(SellerRequestEnums).join('، ')} باشد`,
-  })
-  status?: SellerRequestEnums;
-
-  @IsOptional()
   @IsNumber({}, { message: 'اولویت باید یک عدد معتبر باشد' })
   @Min(1, { message: 'اولویت حداقل باید ۱ باشد' })
   @Max(10, { message: 'اولویت حداکثر باید ۱۰ باشد' })
   @Type(() => Number)
   priority?: number;
-
-  @IsOptional()
-  @IsString({ message: 'نظر ادمین باید یک متن معتبر باشد' })
-  @MinLength(1, { message: 'نظر ادمین نمی‌تواند خالی باشد' })
-  adminComment?: string;
 }
