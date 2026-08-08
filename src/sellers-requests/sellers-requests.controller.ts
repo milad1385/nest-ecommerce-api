f044561930af070012d55328244b7b74641f7b06
Milad salamian <miladsalami1385@gmail.com>
2026-08-08 12:24:37 +0330

    add create seller request controller

--- src/sellers-requests/sellers-requests.controller.ts
+++ src/sellers-requests/sellers-requests.controller.ts
@@ -1,15 +1,49 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Res,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
 import { SellersRequestsService } from './sellers-requests.service';
 import { CreateSellersRequestDto } from './dto/create-sellers-request.dto';
 import { UpdateSellersRequestDto } from './dto/update-sellers-request.dto';
+import type { Response } from 'express';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UserRoleEnums } from 'src/users/enums/userRoleEnums';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
 
 @Controller('sellers-requests')
 export class SellersRequestsController {
-  constructor(private readonly sellersRequestsService: SellersRequestsService) {}
+  constructor(
+    private readonly sellersRequestsService: SellersRequestsService,
+  ) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRoleEnums.SELLER, UserRoleEnums.ADMIN)
   @Post()
-  create(@Body() createSellersRequestDto: CreateSellersRequestDto) {
-    return this.sellersRequestsService.create(createSellersRequestDto);
+  async create(
+    @Res() res: Response,
+    @Body() createSellersRequestDto: CreateSellersRequestDto,
+    @GetUser('id') userId: number,
+  ) {
+    const sellerReqeust = await this.sellersRequestsService.create(
+      createSellersRequestDto,
+      userId,
+    );
+
+    return res.status(HttpStatus.CREATED).json({
+      statusCode: HttpStatus.CREATED,
+      message: 'درخواست فروشنده با موفقیت ثبت شد',
+      data: sellerReqeust,
+    });
   }
 
   @Get()
@@ -23,7 +57,10 @@ export class SellersRequestsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSellersRequestDto: UpdateSellersRequestDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateSellersRequestDto: UpdateSellersRequestDto,
+  ) {
     return this.sellersRequestsService.update(+id, updateSellersRequestDto);
   }
 
