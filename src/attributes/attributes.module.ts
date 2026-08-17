c81d9582ab40f5a73b2e25a904e3acf28ec78bf8
Milad salamian <miladsalami1385@gmail.com>
2026-08-17 13:03:09 +0330

    add attributes module

--- src/attributes/attributes.module.ts
+++ src/attributes/attributes.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AttributesService } from './attributes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AttributesController } from './attributes.controller';
+import { AttributesService } from './attributes.service';
+import { AttributeValue } from './entities/attribute-value.entity';
+import { Attribute } from './entities/attribute.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Attribute, AttributeValue])],
   controllers: [AttributesController],
   providers: [AttributesService],
+  exports: [AttributesService],
 })
 export class AttributesModule {}
