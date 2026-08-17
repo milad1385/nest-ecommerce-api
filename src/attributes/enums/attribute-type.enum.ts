757635c6a9f8f79fc565613ad13dcdd19eeecc05
Milad salamian <miladsalami1385@gmail.com>
2026-08-17 11:14:07 +0330

    add attribute type enums

---
+++ src/attributes/enums/attribute-type.enum.ts
@@ -0,0 +1,9 @@
+export enum AttributeTypeEnum {
+  TEXT = 'text',
+  NUMBER = 'number',
+  SELECT = 'select',
+  MULTISELECT = 'multiselect',
+  BOOLEAN = 'boolean',
+  COLOR = 'color',
+  DATE = 'date',
+}
