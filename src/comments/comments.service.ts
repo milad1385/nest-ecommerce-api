61e00fecaaac025a2018ec8841e76bbf3b9e3e84
Milad salamian <miladsalami1385@gmail.com>
2026-08-18 10:35:48 +0330

    add create comment service

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Comment } from './entities/comment.entity';
+import { Product } from 'src/products/entities/proudct.entity';
+import { Seller } from 'src/sellers/entities/seller.entity';
+import { User } from 'src/users/entities/user.entity';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { FilterCommentDto } from './dto/filter-comment.dto';
+import { CommentStatusEnum } from './enums/comment-status.enum';
+import { ProductsService } from 'src/products/products.service';
+import { SellersService } from 'src/sellers/sellers.service';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class CommentsService {
-  create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
-  }
+  constructor(
+    @InjectRepository(Comment)
+    private commentRepository: Repository<Comment>,
+    private readonly productService: ProductsService,
+    private readonly sellersService: SellersService,
+    private readonly usersService: UsersService,
+  ) {}
 
-  findAll() {
-    return `This action returns all comments`;
-  }
+  async create(userId: number, dto: CreateCommentDto): Promise<Comment> {
+    const user = await this.usersService.findOne(userId);
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
-  }
+    const product = await this.productService.findOneById(dto.product_id);
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
-    return `This action updates a #${id} comment`;
-  }
+    let seller: Seller | null = null;
+    if (dto.seller_id) {
+      seller = await this.sellersService.findOne(dto.seller_id);
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} comment`;
+    const comment = this.commentRepository.create({
+      content: dto.content,
+      rating: dto.rating,
+      user,
+      product,
+      seller,
+      status: CommentStatusEnum.PENDING,
+    });
+
+    return this.commentRepository.save(comment);
   }
 }
