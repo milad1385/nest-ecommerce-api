48cfe35fd8350157895c3d1d923e280b30a70241
Milad salamian <miladsalami1385@gmail.com>
2026-08-18 10:10:46 +0330

    add update comment dto

--- src/comments/dto/update-comment.dto.ts
+++ src/comments/dto/update-comment.dto.ts
@@ -1,4 +1,33 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCommentDto } from './create-comment.dto';
+import {
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+import { CommentStatusEnum } from '../enums/comment-status.enum';
+import { Type } from 'class-transformer';
 
-export class UpdateCommentDto extends PartialType(CreateCommentDto) {}
+export class UpdateCommentDto {
+  @IsOptional()
+  @IsString({ message: 'متن کامنت باید یک متن باشد' })
+  content?: string;
+
+  @IsOptional()
+  @IsNumber({}, { message: 'امتیاز باید یک عدد باشد' })
+  @Min(1, { message: 'امتیاز حداقل باید ۱ باشد' })
+  @Max(5, { message: 'امتیاز حداکثر باید ۵ باشد' })
+  @Type(() => Number)
+  rating?: number;
+
+  @IsOptional()
+  @IsEnum(CommentStatusEnum, {
+    message: `وضعیت باید یکی از مقادیر ${Object.values(CommentStatusEnum).join('، ')} باشد`,
+  })
+  status?: CommentStatusEnum;
+
+  @IsOptional()
+  @IsString({ message: 'پاسخ ادمین باید یک متن باشد' })
+  adminReply?: string;
+}
