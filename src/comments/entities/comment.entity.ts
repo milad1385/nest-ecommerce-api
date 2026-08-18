ebb922dcb08b8ae436476a0b997d52dd238ea323
Milad salamian <miladsalami1385@gmail.com>
2026-08-18 10:09:48 +0330

    add comment entity

--- src/comments/entities/comment.entity.ts
+++ src/comments/entities/comment.entity.ts
@@ -1,1 +1,62 @@
-export class Comment {}
+import { Product } from 'src/products/entities/proudct.entity';
+import { Seller } from 'src/sellers/entities/seller.entity';
+import { User } from 'src/users/entities/user.entity';
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from 'typeorm';
+import { CommentStatusEnum } from '../enums/comment-status.enum';
+
+@Entity({ name: 'comments' })
+export class Comment {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @Column({ type: 'tinyint', default: 5 })
+  rating: number;
+
+  @Column({
+    type: 'enum',
+    enum: CommentStatusEnum,
+    default: CommentStatusEnum.PENDING,
+  })
+  status: CommentStatusEnum;
+
+
+  @Column({ nullable: true })
+  approvedAt: Date;
+
+  @Column({ nullable: true })
+  adminReply: string;
+
+
+  @ManyToOne(() => User, (user) => user.comments, {
+    onDelete: 'CASCADE',
+  })
+  user: User;
+
+
+  @ManyToOne(() => Product, (product) => product.comments, {
+    onDelete: 'CASCADE',
+  })
+  product: Product;
+
+  @ManyToOne(() => Seller, (seller) => seller.comments, {
+    onDelete: 'CASCADE',
+    nullable: true,
+  })
+  seller: Seller | null;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
