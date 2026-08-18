47ea184f544ee5b66ff98328201b853dcaf26fed
Milad salamian <miladsalami1385@gmail.com>
2026-08-18 10:41:37 +0330

    add product , seller and user module in comments module

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { ProductsModule } from 'src/products/products.module';
+import { SellersModule } from 'src/sellers/sellers.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [ProductsModule, SellersModule, UsersModule],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
