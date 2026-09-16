7af48e65d30ad51b839333bcaf30fa95b49e69e0
Milad salamian <miladsalami1385@gmail.com>
2026-09-16 15:30:26 +0330

    add bookmark entity

--- src/bookmarks/entities/bookmark.entity.ts
+++ src/bookmarks/entities/bookmark.entity.ts
@@ -1,1 +1,41 @@
-export class Bookmark {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  Index,
+  Unique,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { Product } from 'src/products/entities/proudct.entity';
+
+@Entity('bookmarks')
+@Unique(['user_id', 'product_id'])
+export class Bookmark {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, (user) => user.bookmarks, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => Product, (product) => product.bookmarks, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'product_id' })
+  product: Product;
+
+  @Column({ type: 'text', nullable: true })
+  note: string | null;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
