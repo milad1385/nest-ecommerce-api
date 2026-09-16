3a0668626a94cdf806b14f2ece2470d08c570390
Milad salamian <miladsalami1385@gmail.com>
2026-09-16 15:30:34 +0330

    add create bookmark dto

--- src/bookmarks/dto/create-bookmark.dto.ts
+++ src/bookmarks/dto/create-bookmark.dto.ts
@@ -1,1 +1,20 @@
-export class CreateBookmarkDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateBookmarkDto {
+  @IsInt({ message: 'آیدی محصول باید عدد باشد' })
+  @IsPositive({ message: 'آیدی محصول باید عدد مثبت باشد' })
+  @IsNotEmpty({ message: 'وارد کردن آیدی محصول الزامی است' })
+  product_id: number;
+
+  @IsOptional()
+  @IsString({ message: 'متن یادداشت باید از نوع متن باشد' })
+  @MaxLength(500)
+  note?: string;
+}
