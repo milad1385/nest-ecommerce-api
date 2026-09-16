4707fb64b35016b0e8c1050d7d41972c849654d5
Milad salamian <miladsalami1385@gmail.com>
2026-09-16 14:38:41 +0330

    delete unneccery dto

--- src/ip-tracker/dto/update-ip-tracker.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateIpTrackerDto } from './create-ip-tracker.dto';
-
-export class UpdateIpTrackerDto extends PartialType(CreateIpTrackerDto) {}
