4276978a8465aece1098485d427915b30a5177e6
Milad salamian <miladsalami1385@gmail.com>
2026-09-16 23:39:20 +0330

    add menus entity

--- src/menus/entities/menu.entity.ts
+++ src/menus/entities/menu.entity.ts
@@ -1,1 +1,36 @@
-export class Menu {}
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from 'typeorm';
+
+@Entity({ name: 'menus' })
+export class Menu {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ unique: true })
+  title: string;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @OneToMany(() => Menu, (menu) => menu.parent, {
+    nullable: true,
+    cascade: true,
+  })
+  subMenus: Menu[];
+
+  @ManyToOne(() => Menu, (menu) => menu.subMenus)
+  parent: Menu | null;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
