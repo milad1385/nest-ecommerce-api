007e7fd29c8c5359942c9698b5d252538f2ba232
Milad salamian <miladsalami1385@gmail.com>
2026-09-16 11:41:36 +0330

    add toJalaliDate func

--- utils/func.ts
+++ utils/func.ts
@@ -13,3 +13,15 @@ export const createPagination = (
     hasPrevious: page > 1,
   };
 };
+
+export function toJalaliDate(date: Date): string {
+  return new Intl.DateTimeFormat('fa-IR', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    timeZone: 'Asia/Tehran',
+  }).format(date);
+}
