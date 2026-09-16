4999eb694428f9f5f77d7e77a17614d78ea05881
Milad salamian <miladsalami1385@gmail.com>
2026-09-16 10:31:03 +0330

    add ip tracket entity

--- src/ip-tracker/entities/ip-tracker.entity.ts
+++ src/ip-tracker/entities/ip-tracker.entity.ts
@@ -1,1 +1,22 @@
-export class IpTracker {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class IpTracker {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  ip: string;
+
+  @Column({ type: 'timestamp' })
+  windowStart: Date;
+
+  @Column()
+  requestCount: number;
+
+  @Column({ default: false })
+  isBlock: boolean;
+
+  @Column({ type: 'timestamp', nullable: true })
+  blockUntil: Date | null;
+}
