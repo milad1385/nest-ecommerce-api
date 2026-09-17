27821cb27510fd92d7593111d01bf5fa02d81f73
Milad salamian <miladsalami1385@gmail.com>
2026-09-17 17:01:31 +0330

    add basket module

--- src/baskets/baskets.module.ts
+++ src/baskets/baskets.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BasketsService } from './baskets.service';
 import { BasketsController } from './baskets.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Basket } from './entities/basket.entity';
+import { ProductSeller } from 'src/sellers/entities/product_seller.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Basket, ProductSeller])],
   controllers: [BasketsController],
   providers: [BasketsService],
 })
