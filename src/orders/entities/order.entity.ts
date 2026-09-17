9cafc91cf276f3fc2ad5afc9344bb9ad580ab4af
Milad salamian <miladsalami1385@gmail.com>
2026-09-18 00:07:30 +0330

    add order entity

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,86 @@
-export class Order {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { OrderItem } from './order-item.entity';
+import { OrderStatus } from '../enums/order-status.enum';
+import { Address } from 'src/address/entities/address.entity';
+
+@Entity({ name: 'orders' })
+export class Order {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @ManyToOne(() => User, (user) => user.orders, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: User | null;
+
+  @ManyToOne(() => Address, {
+    onDelete: 'SET NULL',
+    nullable: true,
+  })
+  @JoinColumn({ name: 'address_id' })
+  address: Address | null;
+
+  @Column({ type: 'int' })
+  total_price: number;
+
+  @Column({ type: 'int', default: 0 })
+  total_discount: number;
+
+  @Column({ type: 'int' })
+  final_price: number;
+
+  @Column({
+    type: 'enum',
+    enum: OrderStatus,
+    default: OrderStatus.PENDING,
+  })
+  status: OrderStatus;
+
+  @Column({ type: 'varchar', length: 50, nullable: true })
+  payment_method: string | null;
+
+  @Column({ type: 'timestamp', nullable: true })
+  paid_at: Date | null;
+
+  @Column({ type: 'varchar', length: 100, unique: true, nullable: true })
+  tracking_code: string | null;
+
+  @Column({ type: 'timestamp', nullable: true })
+  cancelled_at: Date | null;
+
+  @Column({ type: 'text', nullable: true })
+  cancel_reason: string | null;
+
+  @Column({ type: 'timestamp', nullable: true })
+  shipped_at: Date | null;
+
+  @Column({ type: 'timestamp', nullable: true })
+  delivered_at: Date | null;
+
+  @Column({ type: 'text', nullable: true })
+  note: string | null;
+
+  @OneToMany(() => OrderItem, (item) => item.order, {
+    cascade: true,
+  })
+  items: OrderItem[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
