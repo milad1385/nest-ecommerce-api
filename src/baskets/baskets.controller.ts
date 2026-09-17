6794b01d1ab8401cd0545e759ee5ad2eabf6e5bb
Milad salamian <miladsalami1385@gmail.com>
2026-09-17 17:01:08 +0330

    add baskets controller

--- src/baskets/baskets.controller.ts
+++ src/baskets/baskets.controller.ts
@@ -1,34 +1,103 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Res,
+  UseGuards
+} from '@nestjs/common';
+import type { Response } from 'express';
+import { GetUser } from '../auth/decorators/get-user.decorator';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { BasketsService } from './baskets.service';
-import { CreateBasketDto } from './dto/create-basket.dto';
-import { UpdateBasketDto } from './dto/update-basket.dto';
+import { AddToBasketDto } from './dto/add-to-basket.dto';
+import { UpdateQuantityDto } from './dto/update-basket.dto';
 
 @Controller('baskets')
+@UseGuards(JwtAuthGuard)
 export class BasketsController {
   constructor(private readonly basketsService: BasketsService) {}
 
   @Post()
-  create(@Body() createBasketDto: CreateBasketDto) {
-    return this.basketsService.create(createBasketDto);
+  async add(
+    @Res() res: Response,
+    @Body() dto: AddToBasketDto,
+    @GetUser('id') userId: number,
+  ) {
+    const result = await this.basketsService.addToBasket(userId, dto);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'محصول به سبد خرید اضافه شد',
+      data: result.data,
+    });
   }
 
   @Get()
-  findAll() {
-    return this.basketsService.findAll();
+  async getBasket(@Res() res: Response, @GetUser('id') userId: number) {
+    const result = await this.basketsService.getUserBasket(userId);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: result.message,
+      data: result.data,
+    });
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.basketsService.findOne(+id);
+  @Get('count')
+  async getCount(@Res() res: Response, @GetUser('id') userId: number) {
+    const result = await this.basketsService.getBasketCount(userId);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: result.message,
+      data: result.data,
+    });
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBasketDto: UpdateBasketDto) {
-    return this.basketsService.update(+id, updateBasketDto);
+  @Patch(':id/quantity')
+  async updateQuantity(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @Body() dto: UpdateQuantityDto,
+    @GetUser('id') userId: number,
+  ) {
+    const result = await this.basketsService.updateQuantity(userId, +id, dto);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'تعداد با موفقیت بروزرسانی شد',
+      data: result.data,
+    });
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.basketsService.remove(+id);
+  async removeItem(
+    @Res() res: Response,
+    @Param('id') id: number,
+    @GetUser('id') userId: number,
+  ) {
+    const result = await this.basketsService.removeItem(userId, +id);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: 'آیتم از سبد خرید حذف شد',
+      data: result.data,
+    });
+  }
+
+  @Delete()
+  async clearBasket(@Res() res: Response, @GetUser('id') userId: number) {
+    const result = await this.basketsService.clearBasket(userId);
+
+    return res.status(HttpStatus.OK).json({
+      statusCode: HttpStatus.OK,
+      message: result.message,
+      data: result.data,
+    });
   }
 }
